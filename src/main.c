66219d46d2a9f291b82a1489ff2ef208b1c49867
Ben Glick <glick@glick.cloud>
2024-11-22 17:43:32 -0800

    fix printing test results

--- src/main.c
+++ src/main.c
@@ -20,9 +20,11 @@
 #include "simple_mapping.h"
 
 
-static void print_test_results(uint64_t test_counter){
+static void print_test_results(uint64_t test_counter, uint64_t test_run){
 	for (uint8_t i=0; i<64; i++){
-		printf("Result of test %hhu was %llu\n", i, (((1<<i) & test_counter) >> i) );
+		if ((((1<<i) & test_run) >> i) == 1){
+			printf("Result of test %hhu was %llu\n", i, (((1<<i) & test_counter) >> i) );
+		}
 	}
 }
 
@@ -32,14 +34,16 @@ int main(){
 	ptw_sim_context_t sim_ctx = {0};
 
 	uint64_t result = 0;
+	uint64_t test_run = 0;
 
 	uint8_t test_counter = 0;
 	printf("Test %hhu is simple mapping test\n", test_counter);
+	test_run |= (1<< test_counter);
 	result |= (run_simple_mapping_test(&sim_ctx) << test_counter);
 	test_counter++;
 
 
-	print_test_results(result);
+	print_test_results(result, test_run);
 
 	return (result == 0);
 }
